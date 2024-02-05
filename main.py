ff4a5f1f3de67eacdd17866900bddb38d2c1524f
Alast0rRL <eshenkove@gmail.com>
2024-02-05 16:34:29 +0500

    1

---
+++ main.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+print("Говорите...")
+def main():            
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)  # Добавлено для улучшения распознавания речи в условиях шума
+        audio = r.listen(source)
+
+    try:
+        print(r.recognize_google(audio, language='ru-RU'))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition не понял аудио")
+    except sr.RequestError as e:
+        print("Не удалось запросить результаты от Google Speech Recognition service; {0}".format(e))
+
+while True:
+    main()
