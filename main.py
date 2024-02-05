b55689b540dccc28e178bc3aef7fdc94fb041e43
Alast0rRL <eshenkove@gmail.com>
2024-02-05 23:35:03 +0500

    working

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import speech_recognition as sr
+import datetime
 
+# Получаем текущую дату
+now = datetime.datetime.now()
+
+# Выводим дату в формате DD-MM-YYYY
 r = sr.Recognizer()
 print("Говорите...\n CTR:+C - Остановка программы")
-
+with open('Logs.txt', 'a') as file:
+            file.write(f"\n{now.strftime("%d-%m-%Y %H:%M")}\n")
 def main():            
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=1)
@@ -10,7 +16,7 @@ def main():
     try:
         text = r.recognize_google(audio, language='ru-RU')
         print(text)
-        if text.lower() == 'стоп':
+        if text.lower() == 'выключить':
             input("Программа остановлена по команде пользователя.")
             return False
         with open('Logs.txt', 'a') as file:
