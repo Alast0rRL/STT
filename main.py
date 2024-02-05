c081d86b86367da1d5ac8d3d05ec63de5c453653
Alast0rRL <eshenkove@gmail.com>
2024-02-05 23:21:28 +0500

    123

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import speech_recognition as sr
-# Открываем файл в режиме добавления
+
 r = sr.Recognizer()
 print("Говорите...\n CTR:+C - Остановка программы")
+
 def main():            
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=1)  # Добавлено для улучшения распознавания речи в условиях шума
+        r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
     try:
-        print(r.recognize_google(audio, language='ru-RU'))
+        text = r.recognize_google(audio, language='ru-RU')
+        print(text)
+        if text.lower() == 'стоп':
+            input("Программа остановлена по команде пользователя.")
+            return False
         with open('Logs.txt', 'a') as file:
-        # Получаем ввод от пользователя
-            text = r.recognize_google(audio, language='ru-RU')
-        # Записываем ввод в файл и переходим на новую строку
             file.write(f"{text}\n")
     except sr.UnknownValueError:
         print("Говорите внятно")
     except sr.RequestError as e:
         print("Не удалось запросить результаты от Speech Recognition service; {0}".format(e))
+    return True
 
-while True:
-    main()
+while main():
+    pass
